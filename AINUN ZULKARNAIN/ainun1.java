f1b4a4185eb2e2dade56c839b0f33232f793d129
Nur Ainun Zulkarnain <62009449+13020180030@users.noreply.github.com>
2020-03-10 15:49:22 +0700

    Add files via upload

---
+++ AINUN ZULKARNAIN/ainun1.java
@@ -0,0 +1,10 @@
+/* 	Nama 	:Nur Ainun Zulkarnain
+	Tanggal :10 maret 2020
+	waktu 	:12.58
+*/
+public class ainun1{
+	public static void main(String[] args)
+	{
+		System.out.print("selamat belajar");
+	}
+}
